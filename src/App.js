bfdc5d847920a21874ac80a1df3e2aa4cc034642
Indira_Matieva <122766793+4553462@users.noreply.github.com>
2023-04-06 16:04:47 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { First } from './components/first/First';
+import { Second } from './components/second/second';
+import Third from './components/third/Third';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+       const user = {name: "Anna", age: 19} 
+  
+    return (
+      <>
+      <First text = {user}/>
+      <Second text= {user}/>
+      <Third/>
+      </>
+    )
 }
 
 export default App;
